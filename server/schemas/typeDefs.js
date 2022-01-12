488f11a4925fe68b017013d8d0ca10e0203d0e6e
Marvin Ren <marvin.ren@yale.edu>
2022-01-12 17:53:00 -0500

    mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,7 @@
 //import the gql tagged template function
 const { gql } = require('apollo-server-express')
-const { User, Thought} = require('../models')
-
+// const { User, Thought} = require('../models')
+// const {Auth} = require('../utils/auth')
 
 //create our typeDefs
 const typeDefs = 
@@ -32,11 +32,27 @@ type User {
 }
 
 type Query {
+    me: User
     users: [User]
     user(username: String!): User
     thoughts(username: String): [Thought]
     thought(_id: ID!): Thought
-}`;
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+`;
+
 
 //export the typeDefs
 module.exports = typeDefs
