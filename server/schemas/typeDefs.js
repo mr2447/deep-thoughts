299a0f8d2512ce63f2dbe36e93314ed32abe7c38
Marvin Ren <marvin.ren@yale.edu>
2022-01-12 05:07:41 -0500

    error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,19 @@
 //import the gql tagged template function
 const { gql } = require('apollo-server-express')
 
+
+type Thought {
+    _id: ID
+    thoughtText: String
+    createdAt: String
+    username: String
+    reactionCount: Int
+}
+
 //create our typeDefs
 const typeDefs = gql`
 type Query {
-    helloWorld: String
+    thoughts: [Thought]
 }`;
 
 //export the typeDefs
