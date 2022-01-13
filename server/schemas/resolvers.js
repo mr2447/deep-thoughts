24da0c1be92755c5976f4501567b0103334a990d
Marvin Ren <marvin.ren@yale.edu>
2022-01-13 05:46:18 -0500

    update

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,13 +4,14 @@ const {signToken} = require('../utils/auth')
 
 const resolvers = {
     Query: {
-        me: async (parent, args) => {
+        me: async (parent, args, context) => {
             if(context.user) {
-                const userData = await User.findOne({})
+                console.log(context.user)
+                const userData = await User.findById(context.user._id)
                 .select('-__v -password')
                 .populate('thoughts')
                 .populate('friends');
-
+                console.log(userData)
                 return userData;
             }
             throw new AuthenticationError('Not logged in')
