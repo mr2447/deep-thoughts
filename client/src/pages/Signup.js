8caeca35f69d63a1ab2aacdcc6a5533fefc92c9c
Marvin Ren <marvin.ren@yale.edu>
2022-01-13 17:40:36 -0500

    parsing'

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,8 +1,12 @@
+//add functionality for capturing form field data from a user and storing it in state using the useState() Hook from React
 import React, { useState } from 'react';
+//take that data on submit and send it to the server throught our mutation
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
-
+  const [addUser, { error }] = useMutation(ADD_USER);
   // update state based on form input changes
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -17,6 +21,16 @@ const Signup = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
   };
+  // use try/catch instead of promises to handle errors
+  try {
+    //execute addUser mutation and pass in variable data from form
+    const { data } = await addUser({
+      variables: { ...formState }
+    });
+    console.log(data);
+  } catch(e) {
+    console.error(e);
+  }
 
   return (
     <main className='flex-row justify-center mb-4'>
@@ -56,6 +70,7 @@ const Signup = () => {
                 Submit
               </button>
             </form>
+            {error && <div>Sign up failed</div>}
           </div>
         </div>
       </div>
